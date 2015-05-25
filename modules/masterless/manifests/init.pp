6f6eaf6d62d5bfb8565ce22f40e47efb83bb1848
Les Aker <me@lesaker.org>
2015-05-25 18:38:15 -0400

    fix parameter

--- modules/masterless/manifests/init.pp
+++ modules/masterless/manifests/init.pp
@@ -7,17 +7,17 @@ class masterless(
 ) {
     file { "$bindir/puppet-run":
         ensure => 'file',
-        source => template('masterless/puppet-run.erb')
+        content => template('masterless/puppet-run.erb')
     }
 
     file { '/etc/systemd/system/puppet-run.service':
         ensure => 'file',
-        source => template('masterless/puppet-run.service.erb')
+        content => template('masterless/puppet-run.service.erb')
     }
 
     file { '/etc/systemd/system/puppet-run.timer':
         ensure => 'file',
-        source => template('masterless/puppet-run.timer.erb')
+        content => template('masterless/puppet-run.timer.erb')
     }
 
     file { '/etc/systemd/system/multi-user.target.wants/puppet-run.timer':

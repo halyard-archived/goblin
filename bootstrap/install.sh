49176e2fe2628665bf4ce1b3b55d6c298a5c56ed
Les Aker <me@lesaker.org>
2020-12-23 16:18:45 -0500

    ugly hack for puppet

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -59,6 +59,7 @@ password="$(head -c256 /dev/urandom | md5sum | head -c24)"
 echo "root:$password" | arch-chroot /mnt chpasswd
 
 echo 'Bootstrap goblin'
+sed -i 's/Arch Linux/ArchLinux/' /mnt/etc/os-release
 curl -sLo /mnt/root/kickstart https://git.io/halyard-kickstart
 arch-chroot /mnt bash /root/kickstart
 
